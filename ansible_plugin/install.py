6d7d0f672f57481390911c468c69ae9f0e5832b0
earthmant <trammell.earthling@gmail.com>
2014-12-15 16:04:19 +0200

    added the fix broken flag to install software-common-properties

--- ansible_plugin/install.py
+++ ansible_plugin/install.py
@@ -29,23 +29,25 @@ from cloudify.decorators import operation
 # Import Cloudify exception
 from cloudify.exceptions import NonRecoverableError
 
-# Runs any Shell Command
-def _run_shell_command(command):
-  ctx.logger.info("Running shell command: {0}".format(command))
-  try:
-    run = subprocess.check_call(
-      command)
-  except subprocess.CalledProcessError:
-  	ctx.logger.error("Unable to run shell command: {0}".format(command))
-  	raise NonRecoverableError("Command failed: {0}".format(command))
-  return run
+# Wraps _install_package()
+@operation
+def install(**kwargs):
+  if "package_name" in kwargs.items():
+    package = package_name
+  else:
+    package = "ansible"
+  _install_package(package)
 
-# Gets the Distrobution
-def _get_distro_version():
-  info = platform.dist()
-  distro = info[0]
-  version = info[1]
-  return distro,version
+# Installs a Package
+def _install_package(package):
+  package_manager = _get_package_manager()
+  ctx.logger.info("Installing {0}".format(package))
+  _update_package_manager(package_manager)
+  _upgrade_package_manager(package_manager)
+  q,y,f = _install_args(package_manager)
+  command = ["sudo",package_manager,"install",package,q,y,f]
+  _run_shell_command(command)
+  _validate_installation(package)
 
 # Returns the Package Manager for the distrobution
 def _get_package_manager():
@@ -62,52 +64,13 @@ def _get_package_manager():
     exit(1)
   return package_manager
 
-# Returns Install Arguments for a Package Manager
-def _install_args(package_manager):
-  if package_manager == "apt-get":
-    quiet_output = "-qq"
-    assume_yes = "-y"
-  if package_manager == "yum":
-    quiet_output = "-q"
-    assume_yes = "-y"
-  return quiet_output,assume_yes
-
-# Upgrade the Package Manager
-def _upgrade_package_manager(package_manager):
-  q,y = _install_args(package_manager)
-  ctx.logger.info("Upgrading {0}".format(package_manager))
-  command = ["sudo",package_manager,"upgrade",y,q,"-f"]
+# Updates a Package Manager
+def _update_package_manager(package_manager):
+  command = ["sudo","apt-get","clean"]
   _run_shell_command(command)
-
-# Installs the EPEL Repo
-def _install_epel_repo():
-  wget_url = "http://dl.fedoraproject.org/pub/epel/5/x86_64/"
-  distro,version = _get_distro_version()
-  if version == "5*":
-    rpm_version = "epel-release-5-4.noarch.rpm"
-  elif version == "6*":
-    rpm_version = "epel-release-6-8.noarch.rpm"
-  elif version == "7*":
-    rpm_version = "epel-release-7-1.noarch.rpm"
-  else:
-  	rpm_version = ""
-  	ctx.logger.error("invalid version of CentOs: {0}".format(version))
-  filename = os.path.join("/tmp",rpm_version)
-  if not os.path.exists(filename):
-    ctx.logger.info("Deleting Existing RPM file: {0}".format(filename))
-    ctx.logger.info("Downloading EPEL RPM: {0}{1}".format(wget_url,rpm_version))
-    urllib.urlretrieve(wget_url + rpm_version, filename)
-  command = ["sudo","rpm","-Uvh","/tmp/" + rpm_version]
-  _run_shell_command(command)
-
-# Installs the PPA repos
-def _install_ppa_repo():
-  # Installs the Software Properties Common Dependency
-  def _install_dependency():
-    command = ["sudo","apt-get","install","software-properties-common","-y"]
-    _run_shell_command(command)
-  _install_dependency()
-  command = ["sudo","apt-add-repository","ppa:ansible/ansible","-y"]
+  _add_repo(package_manager)
+  ctx.logger.info("Updating {0}".format(package_manager))
+  command = ["sudo",package_manager,"update"]
   _run_shell_command(command)
 
 # Decides which repos to install
@@ -120,14 +83,74 @@ def _add_repo(package_manager):
   else:
     ctx.logger.error("Not yum or apt-get.")
 
-# Updates a Package Manager
-def _update_package_manager(package_manager):
-  command = ["sudo","apt-get","clean"]
+# Installs the EPEL Repo
+def _install_epel_repo():
+  wget_url = "http://dl.fedoraproject.org/pub/epel/5/x86_64/"
+  distro,version = _get_distro_version()
+  if version == "5*":
+    rpm_version = "epel-release-5-4.noarch.rpm"
+  elif version == "6*":
+    rpm_version = "epel-release-6-8.noarch.rpm"
+  elif version == "7*":
+    rpm_version = "epel-release-7-1.noarch.rpm"
+  else:
+    rpm_version = ""
+    ctx.logger.error("invalid version of CentOs: {0}".format(version))
+  filename = os.path.join("/tmp",rpm_version)
+  if not os.path.exists(filename):
+    ctx.logger.info("Deleting Existing RPM file: {0}".format(filename))
+    ctx.logger.info("Downloading EPEL RPM: {0}{1}".format(wget_url,rpm_version))
+    urllib.urlretrieve(wget_url + rpm_version, filename)
+  command = ["sudo","rpm","-Uvh","/tmp/" + rpm_version]
   _run_shell_command(command)
-  _add_repo(package_manager)
-  ctx.logger.info("Updating {0}".format(package_manager))
-  command = ["sudo",package_manager,"update"]
+
+# Installs the PPA repos
+def _install_ppa_repo():
+  # Installs the Software Properties Common Dependency
+  def _install_dependency():
+    q,y,f = _install_args(package_manager)
+    command = ["sudo","apt-get","install","software-properties-common",q,y,f]
+    _run_shell_command(command)
+  _install_dependency()
+  command = ["sudo","apt-add-repository","ppa:ansible/ansible","-y"]
   _run_shell_command(command)
+
+# Upgrade the Package Manager
+def _upgrade_package_manager(package_manager):
+  q,y,f = _install_args(package_manager)
+  ctx.logger.info("Upgrading {0}".format(package_manager))
+  command = ["sudo",package_manager,"upgrade",y,q,f]
+  _run_shell_command(command)
+
+# Returns Install Arguments for a Package Manager
+def _install_args(package_manager):
+  if package_manager == "apt-get":
+    quiet_output = "-qq"
+    assume_yes = "-y"
+    fix_broken = ""
+  if package_manager == "yum":
+    quiet_output = "-q"
+    assume_yes = "-y"
+    fix_broken = "-f"
+  return quiet_output,assume_yes,fix_broken
+
+# Gets the Distrobution
+def _get_distro_version():
+  info = platform.dist()
+  distro = info[0]
+  version = info[1]
+  return distro,version
+
+# Runs any Shell Command
+def _run_shell_command(command):
+  ctx.logger.info("Running shell command: {0}".format(command))
+  try:
+    run = subprocess.check_call(
+      command)
+  except subprocess.CalledProcessError:
+    ctx.logger.error("Unable to run shell command: {0}".format(command))
+    raise NonRecoverableError("Command failed: {0}".format(command))
+  return run
 
 # validate the installation
 def _validate_installation(package):
@@ -139,21 +162,4 @@ def _validate_installation(package):
   else:
     ctx.logger.info("Installation was successful")
 
-# Installs a Package
-def install_package(package):
-  package_manager = _get_package_manager()
-  ctx.logger.info("Installing {0}".format(package))
-  _update_package_manager(package_manager)
-  _upgrade_package_manager(package_manager)
-  q,y = _install_args(package_manager)
-  command = ["sudo",package_manager,"install",package,q,y]
-  _run_shell_command(command)
-  _validate_installation(package)
 
-@operation
-def install(**kwargs):
-  if "package_name" in kwargs:
-    package = package_name
-  else:
-    package = "ansible"
-  install_package(package)
