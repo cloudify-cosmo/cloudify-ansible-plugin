a85e48472598abdb62c8f4ed909bfde6fc76f40b
earthmant <trammell.earthling@gmail.com>
2014-12-16 17:54:47 +0200

    updating install.py before changing it

--- ansible_plugin/tasks.py
+++ ansible_plugin/tasks.py
@@ -14,50 +14,56 @@
 #    * limitations under the License.
 
 # for running shell commands
-import os,subprocess
+import subprocess
 
 # ctx is imported and used in operations
 from cloudify import ctx
 from cloudify import exceptions
 from cloudify.decorators import operation
+from cloudify.exceptions import NonRecoverableError
 
-# Runs any Shell Command
+
 def _run_shell_command(command):
-  ctx.logger.info("Running shell command: {0}".format(command))
-  try:
-    run = subprocess.check_call(
-      command)
-  except subprocess.CalledProcessError:
-    ctx.logger.error("Unable to run shell command: {0}".format(command))
-    raise NonRecoverableError("Command failed: {0}".format(command))
-  return run
+    """this runs a shell command.
+    """
+
+    ctx.logger.info("Running shell command: {0}".format(command))
+    try:
+        run = subprocess.check_call(command)
+    except subprocess.CalledProcessError:
+        ctx.logger.error("Unable to run shell command: {0}".format(command))
+        raise NonRecoverableError("Command failed: {0}".format(command))
+    return run
+
 
 @operation
-def run_playbook(path,arguments,**kwargs):
+def run_playbook(path, arguments, **kwargs):
+    """runs a playbook
+    """
 
-  '''
-    runs a playbook
-  '''
+    command = ['ansible-playbook', path, arguments]
 
-  command = ['ansible-playbook',path,arguments]
+    ctx.logger.info("Running Playbook: [Shell Command]: {0}".format(command))
 
-  ctx.logger.info("Running Playbook: [Shell Command]: {0}".format(command))
+    _run_shell_command(command)
 
-  _run_shell_command(command)
 
 @operation
-def download_playbook(playbook_file,target_file,**kwargs):
+def download_playbook(playbook_file, target_file, **kwargs):
+    """adds a playbook file in /etc/ansible with content {entry}
+    """
 
-  '''
-    adds a playbook file in /etc/ansible with content {entry}
-  '''
+    ctx.logger.debug('getting Playbook file...')
 
-  ctx.logger.debug('getting Playbook file...')
+    try:
+        blueprint, resource_path, log, target_path = ctx.download_resource(
+            playbook_file, target_file)
+    except Exception as e:
+        raise exceptions.NonRecoverableError(
+            "Could not get '{0}' ({1}: {2})".format(
+                playbook_file, type(e).__name__, e))
 
-  try:
-    blueprint,resource_path,log,target_path = ctx.download_resource(playbook_file,target_file)
-  except Exception as e:
-    raise exceptions.NonRecoverableError("Could not get '{0}' ({1}: {2})".format(playbook_file,type(e).__name__, e))
-
-  ctx.logger.info("Blueprint: {0}, Log: {1}, Downloaded playbook file '{2}' to: {3}. ".format(blueprint,resource_path,log,target_path))
-
+    ctx.logger.info(
+        "Blueprint: {0}, Log: {1}, Downloaded playbook "
+        " file '{2}' to: {3}. ".format(
+            blueprint, resource_path, log, target_path))
