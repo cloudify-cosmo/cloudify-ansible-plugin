4c50ad88a346ba083169818333959ff6f97815f6
earthmant <trammell.earthling@gmail.com>
2014-12-16 14:36:18 +0200

    updated operations in tasks

--- ansible_plugin/tasks.py
+++ ansible_plugin/tasks.py
@@ -14,108 +14,50 @@
 #    * limitations under the License.
 
 # for running shell commands
-import subprocess
-# for walking files and directories
-import os
+import os,subprocess
 
 # ctx is imported and used in operations
 from cloudify import ctx
-
-# put the operation decorator on any function that is a task
+from cloudify import exceptions
 from cloudify.decorators import operation
 
-ansible_home = "/etc/ansible/"
-
-def _write_to_file(path,filename,entry):
-  if not os.path.exists(path):
-    os.makedirs()
-  path_to_file = os.path.join(path,filename)
-  if not os.path.exists(path_to_file):
-    f = open(path_to_file,"w")
-    f.write(entry)
-  else:
-    f = open(path_to_file,"a")
-    f.write(entry)
-  f.close()
-
-def _search_file_string(path,filename,string):
-  thefile = os.path.join(path,filename)
-  with open(thefile) as search:
-    for line in search:
-      line = line.rstrip()
-      if line == string:
-        return True
-      else:
-        return False
-
-def add_line_to_location(path,filename,line,string):
-  success = False
-  new_file = os.path.join('/tmp',filename+".temp")
-  old_file = os.path.join(path,filename)
-  with open(new_file,'w') as outfile:
-    try:
-      with open(old_file,'r') as infile:
-        rows = iter(infile)
-        for row in rows:
-          if row == line:
-            outfile.write(row)
-            outfile.write(new_line)
-            success = True
-          else:
-            outfile.write(row)
-      infile.close()
-    except IOError as e:
-      success = False
-  outfile.close()
-  if success == True:
-    shutil.copyfile(new_file,old_file)
-    return success
-  else:
-    return success
+# Runs any Shell Command
+def _run_shell_command(command):
+  ctx.logger.info("Running shell command: {0}".format(command))
+  try:
+    run = subprocess.check_call(
+      command)
+  except subprocess.CalledProcessError:
+    ctx.logger.error("Unable to run shell command: {0}".format(command))
+    raise NonRecoverableError("Command failed: {0}".format(command))
+  return run
 
 @operation
-def add_host_to_inventory(name,group,inventory,**kwargs):
+def run_playbook(path,arguments,**kwargs):
+
   '''
-    this adds a host role, which may be a geographic or application or contextual role, to the specified inventory (staging, production).
+    runs a playbook
   '''
-  if _search_file_string(ansible_home,inventory,name):
-    ctx.logger.error("Unable to add {0} to {1}. Host already exists in this inventory.".format(name,inventory))
-  else:
-    _write_to_file(ansible_home,inventory,name)
+
+  command = ['ansible-playbook',path,arguments]
+
+  ctx.logger.info("Running Playbook: [Shell Command]: {0}".format(command))
+
+  _run_shell_command(command)
 
 @operation
-def add_host_to_group(name,group,inventory,**kwargs):
-  '''
-    this puts a host under a group in inventory file
-  '''
-  group = "\n[" + group + "]\n"
-  name = name + '\n'
-  if add_line_to_location(ansible_home,inventory,group,name):
-    '''
-      if the group already exists in the inventory, the host will be added and add_line_to_location will return True. Otherwise, the group is added and the host under it
-    '''
-    ctx.logger.info("Added new host {0} under {1} in {2}.".format(name,group,inventory))
-  else:
-    new_line = group + name
-    _write_to_file(ansible_home,inventory,new_line)
-    ctx.logger.info("Added new host {0} under {1} in new file {2}.".format(name,group,inventory))
+def download_playbook(playbook_file,target_file,**kwargs):
 
-@operation
-def add_playbook(client_path,ansible_home):
   '''
     adds a playbook file in /etc/ansible with content {entry}
   '''
-  path = ctx.downloadresource(client_path,ansible_home)
-  ctx.logger.info("Added file: {0}".format(path))
 
-def run_playbook(playbook,**kwargs):
-  '''
-    runs ad-hoc command. provide existing ansible_playbook and any arguments,
-    i.e. alternative module directories (--module-path,"/usr/share/ansible")
-    i.e. alternative inventory hosts file (--inventory, "/usr/share/ansible/hosts")
-  '''
-  command = ["ansible_playbook",playbook]
-  for key,value in kwargs.iteritems():
-    command.append(key)
-    command.append(value)
-  _run_shell_command(command)
+  ctx.logger.debug('getting Playbook file...')
+
+  try:
+    blueprint,resource_path,log,target_path = ctx.download_resource(playbook_file,target_file)
+  except Exception as e:
+    raise exceptions.NonRecoverableError("Could not get '{0}' ({1}: {2})".format(playbook_file,type(e).__name__, e))
+
+  ctx.logger.info("Blueprint: {0}, Log: {1}, Downloaded playbook file '{2}' to: {3}. ".format(blueprint,resource_path,log,target_path))
+
