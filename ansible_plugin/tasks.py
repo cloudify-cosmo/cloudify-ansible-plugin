9031205f6951f5d2cddb049203239743ddfc05cb
earthmant <trammell.earthling@gmail.com>
2014-12-18 13:09:22 +0200

    added get_playbook,run_ansible

--- ansible_plugin/tasks.py
+++ ansible_plugin/tasks.py
@@ -22,17 +22,29 @@ from cloudify import exceptions
 from cloudify.decorators import operation
 from cloudify.exceptions import NonRecoverableError
 
+# for ansible api
+import ansible.runner
+
+# for handling files
+from urllib2 import Request, urlopen, URLError, HTTPError
+from shutil import copy, Error
+import os.path as ospath
+from os import makedirs
+
 
 def _run_shell_command(command):
     """this runs a shell command.
     """
 
-    ctx.logger.info("Running shell command: {0}".format(command))
+    ctx.logger.info("Running shell command: {0}"
+                    .format(command))
     try:
         run = subprocess.check_call(command)
     except subprocess.CalledProcessError:
-        ctx.logger.error("Unable to run shell command: {0}".format(command))
-        raise NonRecoverableError("Command failed: {0}".format(command))
+        ctx.logger.error("Unable to run shell command: {0}"
+                         .format(command))
+        raise NonRecoverableError("Command failed: {0}"
+                                  .format(command))
     return run
 
 
@@ -43,27 +55,179 @@ def run_playbook(path, arguments, **kwargs):
 
     command = ['ansible-playbook', path, arguments]
 
-    ctx.logger.info("Running Playbook: [Shell Command]: {0}".format(command))
+    ctx.logger.info("Running Playbook: [Shell Command]: {0}"
+                    .format(command))
 
     _run_shell_command(command)
 
 
 @operation
-def download_playbook(playbook_file, target_file, **kwargs):
+def get_playbook(target_file, **kwargs):
     """adds a playbook file in /etc/ansible with content {entry}
     """
 
-    ctx.logger.debug('getting Playbook file...')
+    if 'playbook_url' in kwargs:
+        url = kwargs['playbook_url']
+        ctx.logger.debug('getting Playbook file...')
+        status = _download_file(url, target_file)
+        if status == 0:
+            ctx.logger.info(
+                "Downloaded blueprint from url: {0}"
+                .format(url))
+        else:
+            ctx.logger.error(
+                "Unable to download blueprint from url: {0}"
+                .format(url))
+    elif 'local_file' in kwargs:
+        file = kwargs['local_file']
+        status = _copy_file(file, target_file)
+    else:
+        ctx.logger.error("No valid file path or url provided.")
+
+
+def _download_file(url, target_file):
+    """ downloads a file from a url and places it in the requested directory
+    """
+
+    req = Request(url)
 
     try:
-        blueprint, resource_path, log, target_path = ctx.download_resource(
-            playbook_file, target_file)
-    except Exception as e:
-        raise exceptions.NonRecoverableError(
-            "Could not get '{0}' ({1}: {2})".format(
-                playbook_file, type(e).__name__, e))
 
-    ctx.logger.info(
-        "Blueprint: {0}, Log: {1}, Downloaded playbook "
-        " file '{2}' to: {3}. ".format(
-            blueprint, resource_path, log, target_path))
+        file = urlopen(req)
+        with open(target_file, 'wb') as local_file:
+            data = file.read()
+            local_file.write(data)
+            local_file.close()
+
+    except HTTPError, e:
+        print('HTTPError {0} {1}'.format(e.code, url))
+        raise exceptions.NonRecoverableError(
+            'Could not get "{0}" ({1}: {2})'.format(
+                url, type(e).__name__, e))
+        return False
+
+    except URLError, e:
+        print('URLError {0} {1}'.format(e.reason, url))
+        raise exceptions.NonRecoverableError(
+            'Could not get "{0}" ({1}: {2})'.format(
+                url, type(e).__name__, e))
+        return False
+
+
+def _copy_file(file, target_file):
+    """ copies 'file' from local machine and moves to
+        target_file
+    """
+
+    try:
+        copy(file, target_file)
+
+    except Error as e:
+        print('Error {0}'.format(e))
+        raise exceptions.NonRecoverableError(
+            'Could not get "{0}" ({1}: {2})'.format(
+                file, type(e).__name__, e))
+        return False
+
+    return True
+
+
+@operation
+def add_host_to_group(host, group, inventory, **kwargs):
+    """
+        this puts a host under a group in inventory file
+    """
+
+    if 'ansible_home' in kwargs:
+        ansible_home = kwargs['ansible_home']
+    else:
+        ansible_home = '/etc/ansible'
+
+    group = '[' + group + ']\n'
+    host = host + '\n'
+    if _add_to_location(ansible_home, inventory, group, host):
+        """
+            if the group already exists in the inventory,
+            the host will be added and add_line_to_location will
+            return True. Otherwise, the group is added and the host under it
+        """
+        print("Added new host {0} under {1} in {2}."
+              .format(host, group, inventory))
+    else:
+        new_line = group + host
+        _write_to_file(ansible_home, inventory, new_line)
+        print("""Added new host {0} under {1} in new file {2}."""
+              .format(host, group, inventory))
+
+
+def _add_to_location(path, filename, search_string, string):
+    """ Find search_string in file and write string directrly
+        below it.
+    """
+
+    success = False
+    new_file = ospath.join('/tmp', filename + ".temp")
+    old_file = ospath.join(path, filename)
+
+    with open(new_file, 'w') as outfile:
+
+        try:
+            with open(old_file, 'r') as lines:
+                for line in lines:
+                    if line == search_string:
+                        outfile.write(line)
+                        outfile.write(string)
+                        success = True
+                    else:
+                        outfile.write(line)
+
+            lines.close()
+        except IOError:
+            success = False
+
+    outfile.close()
+
+    if success is True:
+        copy(new_file, old_file)
+        return success
+    else:
+        return success
+
+
+def _write_to_file(path, filename, entry):
+    """ writes a entry to a file
+    """
+    if not ospath.exists(path):
+        makedirs()
+    path_to_file = ospath.join(path, filename)
+    if not ospath.exists(path_to_file):
+        f = open(path_to_file, 'w')
+        f.write(entry)
+    else:
+        f = open(path_to_file, 'a')
+        f.write(entry)
+    f.close()
+
+
+@operation
+def run_ansible(**kwargs):
+    """ Makes an API call to Ansible Runner.
+        For a list of possible arguments, see:
+        https://github.com/ansible/ansible/
+        blob/devel/lib/ansible/runner/__init__.py
+    """
+    ctx.logger.info("running ansible: ")
+    del kwargs['ctx']
+    runner = ansible.runner.Runner(**kwargs)
+    results = runner.run()
+    _log_results(results)
+
+
+def _log_results(results):
+    for (hostname, result) in results['contacted'].items():
+        if not 'failed' in result:
+            ctx.logger.info('{0} >>>> {1}'.format(hostname, result))
+        elif 'failed' in result:
+            ctx.logger.error('{0} >>>> {1}'.format(hostname, result))
+    for (hostname, result) in results['dark'].items():
+        ctx.logger.error('{0} >>>>> {1}'.format(hostname, result))
