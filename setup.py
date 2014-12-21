6e8f6e156e395f8b04f610f57f5f3ef5d5bd07b5
EarthmanT <trammell.earthling@gmail.com>
2014-12-21 15:48:30 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -26,6 +26,6 @@ setup(
     license='LICENSE',
     zip_safe=False,
     install_requires=["cloudify-plugins-common==3.1", "ansible"],
-    dependency_links=["git://github.com/EarthmanT/ansible.git"],
+    dependency_links=["http://github.com/EarthmanT/ansible/tarball/master#egg=devel"],
     test_requires=["cloudify-dsl-parser==3.1", "nose"]
 )
