2a4843c371ec5a18d5c1a2cf030c049a966bb012
earthmant <trammell.earthling@gmail.com>
2014-12-28 13:15:10 +0200

    rolled back to old version

--- ansible_plugin/tests/test_plugin.py
+++ ansible_plugin/tests/test_plugin.py
@@ -19,19 +19,25 @@ import unittest
 
 from cloudify.workflows import local
 
+
 class TestPlugin(unittest.TestCase):
 
     def setUp(self):
-
         # build blueprint path
         blueprint_path = os.path.join(os.path.dirname(__file__),
-                                      'blueprint', 'test_configure.yaml')
+                                      'blueprint', 'blueprint.yaml')
+
+        # inject input from test
+        inputs = {
+            'test_input': 'new_test_input'
+        }
 
         # setup local workflow execution environment
         self.env = local.init_env(blueprint_path,
-                                  name=self._testMethodName)
+                                  name=self._testMethodName,
+                                  inputs=inputs)
 
-    def test_configure(self):
+    def test_my_task(self):
 
         # execute install workflow
         self.env.execute('install', task_retries=0)
