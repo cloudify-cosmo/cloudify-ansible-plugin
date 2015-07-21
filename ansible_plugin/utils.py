7b5644f20daf76310b9f987720ae19609ffcc21e
earthmant <earthmant@icloud.com>
2015-07-21 13:11:10 +0300

    update

--- ansible_plugin/utils.py
+++ ansible_plugin/utils.py
@@ -69,7 +69,7 @@ def get_agent_user(user=None):
             ctx.instance.runtime_properties['user'] = user
         else:
             user = ctx.instance.runtime_properties['user']
-    elif if 'user' not in ctx.instance.runtime_properties:
+    elif 'user' not in ctx.instance.runtime_properties:
         ctx.instance.runtime_properties['user'] = user
 
     return user
