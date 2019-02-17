50311a565f5801124e74ae1b36174d66327cba09
earthmant <trammell@cloudify.co>
2019-02-17 13:56:58 +0200

    add clearwater

--- Vagrantfile
+++ Vagrantfile
@@ -3,17 +3,42 @@ Vagrant.configure("2") do |config|
     web.vm.box = "centos/7"
     web.vm.network "forwarded_port", guest: 8000, host: 8000
     web.vm.network :private_network, ip: "11.0.0.7"
-    web.vm.provision "file", source: "~/.ssh/id_rsa.pub", destination: "~/.ssh/me.pub"
   end
   config.vm.define "db" do |db|
     db.vm.box = "centos/7"
     db.vm.network "forwarded_port", guest: 3306, host: 3306
     db.vm.network :private_network, ip: "11.0.0.8"
-    db.vm.provision "file", source: "~/.ssh/id_rsa.pub", destination: "~/.ssh/me.pub"
   end
   config.vm.define "vpn" do |vpn|
     vpn.vm.box = "ubuntu/trusty64"
     vpn.vm.network :private_network, ip: "11.0.0.9"
-    vpn.vm.provision "file", source: "~/.ssh/id_rsa.pub", destination: "~/.ssh/me.pub"
+  end
+  config.vm.define "ellis" do |ellis|
+    ellis.vm.box = "ubuntu/trusty64"
+    ellis.vm.network :private_network, ip: "11.0.0.10"
+  end
+  config.vm.define "bono" do |bono|
+    bono.vm.box = "ubuntu/trusty64"
+    bono.vm.network :private_network, ip: "11.0.0.11"
+  end
+  config.vm.define "sprout" do |sprout|
+    sprout.vm.box = "ubuntu/trusty64"
+    sprout.vm.network :private_network, ip: "11.0.0.12"
+  end
+  config.vm.define "homer" do |homer|
+    homer.vm.box = "ubuntu/trusty64"
+    homer.vm.network :private_network, ip: "11.0.0.13"
+  end
+  config.vm.define "homestead" do |homestead|
+    homestead.vm.box = "ubuntu/trusty64"
+    homestead.vm.network :private_network, ip: "11.0.0.14"
+  end
+  config.vm.define "ralf" do |ralf|
+    ralf.vm.box = "ubuntu/trusty64"
+    ralf.vm.network :private_network, ip: "11.0.0.15"
+  end
+  config.vm.define "bind" do |bind|
+    bind.vm.box = "ubuntu/trusty64"
+    bind.vm.network :private_network, ip: "11.0.0.16"
   end
 end
--- cloudify_ansible/tests/test_workflow.py
+++ cloudify_ansible/tests/test_workflow.py
@@ -21,14 +21,6 @@ from cloudify.test_utils import workflow_test
 from cloudify_ansible_sdk.tests import AnsibleTestBase, mock_sources_dict
 
 
-def load_new_vagrant_env(boxes):
-    for box in boxes:
-        subprocess.call("vagrant destroy -f {0}".format(box),
-                        cwd=_plugin_directory, shell=True)
-        subprocess.call("vagrant up {0}".format(box),
-                        cwd=_plugin_directory, shell=True)
-
-
 # This just gives us a path to the setup.py directory.
 _plugin_directory = \
     '/{0}'.format(
@@ -36,21 +28,43 @@ _plugin_directory = \
             path.abspath(path.dirname(__file__)).split('/')[1:-2]
         )
     )
+
 _blueprint_path = \
-   path.join(_plugin_directory, 'examples/hosts-input-blueprint.yaml')
-
+   path.join(_plugin_directory,
+             'examples/hosts-input-blueprint.yaml')
 _compute_blueprint_path = \
-   path.join(_plugin_directory, 'examples/compute-blueprint.yaml')
-
+   path.join(
+       _plugin_directory,
+       'examples/compute-blueprint.yaml')
 _relationships_blueprint = \
-    path.join(_plugin_directory, 'examples/relationships-blueprint.yaml')
-
+    path.join(_plugin_directory,
+              'examples/relationships-blueprint.yaml')
 _another_relationships_blueprint = \
     path.join(_plugin_directory,
               'examples/another-relationships-blueprint.yaml')
-
 _openvpn_blueprint = \
-   path.join(_plugin_directory, 'examples/openvpn-blueprint.yaml')
+    path.join(_plugin_directory,
+              'examples/openvpn-blueprint.yaml')
+_clearwater_blueprint = \
+    path.join(_plugin_directory,
+              'examples/clearwater-blueprint.yaml')
+
+
+def load_new_vagrant_env(boxes=None):
+    boxes = boxes or []
+    if not boxes:
+        subprocess.call("vagrant destroy -f",
+                        cwd=_plugin_directory, shell=True)
+        subprocess.call("vagrant up",
+                        cwd=_plugin_directory, shell=True)
+    for box in boxes:
+        subprocess.call("vagrant destroy -f {0}".format(box),
+                        cwd=_plugin_directory, shell=True)
+        subprocess.call("vagrant up {0}".format(box),
+                        cwd=_plugin_directory, shell=True)
+
+
+load_new_vagrant_env()
 
 web_private_key_path = \
     '.vagrant/machines/{0}/virtualbox/private_key'.format('web')
@@ -58,11 +72,20 @@ db_private_key_path = \
     '.vagrant/machines/{0}/virtualbox/private_key'.format('db')
 openvpn_private_key_path = \
     '.vagrant/machines/{0}/virtualbox/private_key'.format('vpn')
-
-subprocess.call("vagrant destroy -f",
-                cwd=_plugin_directory, shell=True)
-subprocess.call("vagrant up",
-                cwd=_plugin_directory, shell=True)
+ellis_private_key_path = \
+    '.vagrant/machines/{0}/virtualbox/private_key'.format('ellis')
+bono_private_key_path = \
+    '.vagrant/machines/{0}/virtualbox/private_key'.format('bono')
+sprout_private_key_path = \
+    '.vagrant/machines/{0}/virtualbox/private_key'.format('sprout')
+homer_private_key_path = \
+    '.vagrant/machines/{0}/virtualbox/private_key'.format('homer')
+homestead_private_key_path = \
+    '.vagrant/machines/{0}/virtualbox/private_key'.format('homestead')
+ralf_private_key_path = \
+    '.vagrant/machines/{0}/virtualbox/private_key'.format('ralf')
+bind_private_key_path = \
+    '.vagrant/machines/{0}/virtualbox/private_key'.format('bind')
 
 if environ.get('TEST_ZPLAYS', False):
     with open(web_private_key_path, 'r') as infile:
@@ -71,10 +94,31 @@ if environ.get('TEST_ZPLAYS', False):
         db_private_key = infile.read()
     with open(openvpn_private_key_path, 'r') as infile:
         openvpn_private_key = infile.read()
+    with open(ellis_private_key_path, 'r') as infile:
+        ellis_private_key = infile.read()
+    with open(bono_private_key_path, 'r') as infile:
+        bono_private_key = infile.read()
+    with open(sprout_private_key_path, 'r') as infile:
+        sprout_private_key = infile.read()
+    with open(homer_private_key_path, 'r') as infile:
+        homer_private_key = infile.read()
+    with open(homestead_private_key_path, 'r') as infile:
+        homestead_private_key = infile.read()
+    with open(ralf_private_key_path, 'r') as infile:
+        ralf_private_key = infile.read()
+    with open(bind_private_key_path, 'r') as infile:
+        bind_private_key = infile.read()
 else:
     web_private_key = None
     db_private_key = None
     openvpn_private_key = None
+    ellis_private_key = None
+    bono_private_key = None
+    sprout_private_key = None
+    homer_private_key = None
+    homestead_private_key = None
+    ralf_private_key = None
+    bind_private_key = None
 
 
 class TestPluginWorkflows(AnsibleTestBase):
@@ -162,18 +206,64 @@ class TestPluginWorkflows(AnsibleTestBase):
                 'ansible_playbook')[0].runtime_properties.keys())
         load_new_vagrant_env(['db', 'web'])
 
-    # TODO: Fix the Ansible Playbook for this test.
-    # @skipUnless(
-    #     environ.get('TEST_ZPLAYS', False),
-    #     reason='This test requires you to run "vagrant up". '
-    #            'And export TEST_ZPLAYS=true')
-    # @workflow_test(
-    #     _openvpn_blueprint,
-    #     inputs={
-    #         'openvpn_private_key': openvpn_private_key})
-    # def test6_openvpn_blueprint(self, cfy_local):
-    #     cfy_local.execute('install', task_retries=0)
-    #     self.assertIn(
-    #         'result',
-    #         cfy_local.storage.get_node_instances(
-    #             'openvpn')[0].runtime_properties.keys())
+    @skipUnless(
+        environ.get('TEST_OPENVPN', False),
+        reason='This test requires you to run "vagrant up". '
+               'And export TEST_OPENVPN=true')
+    @workflow_test(
+        _openvpn_blueprint,
+        inputs={
+            'openvpn_private_key': openvpn_private_key})
+    def test6_openvpn_blueprint(self, cfy_local):
+        cfy_local.execute('install', task_retries=0)
+        self.assertIn(
+            'result',
+            cfy_local.storage.get_node_instances(
+                'openvpn')[0].runtime_properties.keys())
+
+    @skipUnless(
+        environ.get('TEST_ZPLAYS', False),
+        reason='This test requires you to run "vagrant up". '
+               'And export TEST_ZPLAYS=true')
+    @workflow_test(
+        _clearwater_blueprint,
+        inputs={
+            'ellis_private_key': ellis_private_key,
+            'bono_private_key': bono_private_key,
+            'sprout_private_key': sprout_private_key,
+            'homer_private_key': homer_private_key,
+            'homestead_private_key': homestead_private_key,
+            'ralf_private_key': ralf_private_key,
+            'bind_private_key': bind_private_key,
+        })
+    def test6_clearwater_blueprint(self, cfy_local):
+        load_new_vagrant_env(['db', 'web'])
+        cfy_local.execute('install', task_retries=0)
+        self.assertIn(
+            'result',
+            cfy_local.storage.get_node_instances(
+                'ellis')[0].runtime_properties.keys())
+        self.assertIn(
+            'result',
+            cfy_local.storage.get_node_instances(
+                'bono')[0].runtime_properties.keys())
+        self.assertIn(
+            'result',
+            cfy_local.storage.get_node_instances(
+                'sprout')[0].runtime_properties.keys())
+        self.assertIn(
+            'result',
+            cfy_local.storage.get_node_instances(
+                'homer')[0].runtime_properties.keys())
+        self.assertIn(
+            'result',
+            cfy_local.storage.get_node_instances(
+                'homestead')[0].runtime_properties.keys())
+        self.assertIn(
+            'result',
+            cfy_local.storage.get_node_instances(
+                'ralf')[0].runtime_properties.keys())
+        self.assertIn(
+            'result',
+            cfy_local.storage.get_node_instances(
+                'bind')[0].runtime_properties.keys())
