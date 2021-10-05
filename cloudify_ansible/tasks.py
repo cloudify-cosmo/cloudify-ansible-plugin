3b4970589a56b0e847b610bc408ba6dd675408c1
EarthmanT <trammell@cloudify.co>
2021-10-04 23:56:37 -0400

    ...

--- cloudify_ansible/tasks.py
+++ cloudify_ansible/tasks.py
@@ -131,9 +131,12 @@ def run(playbook_args, ansible_env_vars, _ctx, **kwargs):
     except ProcessException as process_error:
         _, _, tb = sys.exc_info()
         if process_error.exit_code in UNREACHABLE_CODES:
+            number = _ctx.operation.retry_number or 1
+            attempts = playbook_args.get(constants.NUMBER_OF_ATTEMPTS, 60)
+            _ctx.logger.error('{} {}'.format(number, attempts))
             utils.raise_if_retry_is_not_allowed(
-                _ctx.operation.retry_number,
-                playbook_args.get(constants.NUMBER_OF_ATTEMPTS))
+                _ctx.operation.retry_number or 1,
+                playbook_args.get(constants.NUMBER_OF_ATTEMPTS, 60))
             raise OperationRetry(
                 "One or more hosts are unreachable.",
                 causes=[exception_to_error_cause(process_error, tb)])
