c36188ad1c32d78194092f894fe889f2beb2468b
EarthmanT <EarthmanT@users.noreply.github.com>
2022-03-08 11:31:09 -0500

    Update pyyaml (#124)
    * update pyyaml
    
    * bump version

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ setup(
     zip_safe=False,
     install_requires=[
         "cloudify-common>=4.5.5",
-        "cloudify-utilities-plugins-sdk>=0.0.40",
+        "cloudify-utilities-plugins-sdk>=0.0.59",
         "ansible==2.9.5",
         "pexpect==4.8.0"
     ]
