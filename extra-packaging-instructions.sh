5b0d8133f3ab1cf5784c4c6c63abc33034084664
EarthmanT <EarthmanT@users.noreply.github.com>
2023-11-07 13:18:28 -0500

    new instructions

--- extra-packaging-instructions.sh
+++ extra-packaging-instructions.sh
@@ -10,6 +10,12 @@ EOF
     touch ./webbrowser/__init__.py
     cat <<EOF > ./webbrowser/__init__.py
 EOF
-    git apply python311.patch
-    echo patch applied
+    git clone https://${SECRET_TOKEN}@github.com/fusion-e/fusion-common.git
+    pushd fusion-common && git checkout rel/magicp1-2.0.0 && popd
+    git clone https://${SECRET_TOKEN}@github.com/fusion-e/fusion-manager.git
+    pushd fusion-manager && git checkout rel/magicp1-2.0.0 && popd
+    git clone https://${SECRET_TOKEN}@github.com/fusion-e/fusion-agent.git
+    pushd fusion-agent && git checkout rel/magicp1-2.0.0 && popd
+    git clone https://github.com/cloudify-incubator/cloudify-utilities-plugins-sdk.git
+    pushd cloudify-utilities-plugins-sdk && git checkout fusion && popd
 fi
